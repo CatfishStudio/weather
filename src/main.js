51d698986aac63265385ff26856871155416043a
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2017-01-14 12:52:44 +0300

    determine the geolocation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-alert("OK!");
+'use strict';
+
+function myGeolocation(){
+    if(!navigator.geolocation) return;
+    
+    function success(position) {
+        var latitude  = position.coords.latitude;
+        var longitude = position.coords.longitude;
+    }
+
+    function error(err) {
+        alert("ERROR(" + err.code + ") : " + err.message);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error);
+}
+
+myGeolocation();
